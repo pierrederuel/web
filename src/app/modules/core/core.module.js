2b26aff276f93c1ff36b2c80dc4ad5cd60a931e7
Pierre Deruel <envoxoo@MacBook-Pro-de-Pierre.local>
2017-11-26 07:58:45 +0100

    Initial commit

---
+++ src/app/modules/core/core.module.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  var core = angular.module('singApp.core', [
+    'singApp.core.utils',
+    'ui.router',
+    'ui.bootstrap',
+    'ngAnimate',
+    'ngStorage',
+    'angular-oauth2'
+  ]);
+
+  //core.constant('API_SERVER', 'http://192.168.0.32:8080');
+  //core.constant('AUTH_SERVER', 'http://192.168.0.32:8080');
+  core.constant('ID_CLIENT', 'eneliaWeb');
+  core.config(appConfig);
+
+  appConfig.$inject = ['$stateProvider', '$urlRouterProvider', 'OAuthProvider', 'OAuthTokenProvider', 'AUTH_SERVER', 'ID_CLIENT'];
+  function appConfig($stateProvider, $urlRouterProvider, OAuthProvider, OAuthTokenProvider, AUTH_SERVER, ID_CLIENT) {
+
+    OAuthProvider.configure({
+      baseUrl: AUTH_SERVER,
+      clientId: ID_CLIENT,
+      clientSecret: 'secret',
+      grantPath: '/oauth/token',
+      revokePath: '/oauth/revoke',
+      clientCredentials: 'header'
+    });
+
+
+  OAuthTokenProvider.configure({
+    name: 'token',
+    options: {
+      secure: false
+    }
+  });
+
+
+
+
+      $stateProvider
+          .state('app', {
+              url: '/app',
+              abstract: true,
+              templateUrl: 'app/modules/core/app.html'
+          });
+
+      $urlRouterProvider.otherwise(function ($injector) {
+          var $state = $injector.get('$state');
+          $state.go('login');
+      });
+  }
+})();
